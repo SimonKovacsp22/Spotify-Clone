79172c5bc8c07c8d13461995827b4c82c7f729cb
SimonKovacsp22 <postcardsent@gmail.com>
2022-05-18 16:07:57 +0200

    update

--- Artist.js
+++ Artist.js
@@ -1,6 +1,6 @@
 let api = "https://striveschool-api.herokuapp.com/api/deezer/artist"
 let endPoint = new URLSearchParams(window.location.search).get("artist");
-let url = api + "/" + 'eminem'
+let url = api + "/" + 'sia'
 
 let songsOl = document.getElementById('songs-ol')
 let artistPickDivNode = document.querySelector('.artist-pick')
@@ -15,6 +15,7 @@ const getArtist = async function (url) {
    let tracklistUrl = artist.tracklist
    let tracks = await getTracklist(tracklistUrl)
    displaySongs(artist,tracks)
+   displayArtistPick(artist,tracks[2].album)
    
    
    
@@ -27,32 +28,46 @@ const displaySongs = function (artist,tracklist){
    
     tracklist.forEach(track => {
         let artistSongLiNode =document.createElement('li')
-        artistSongLiNode.className = 'd-flex'
+        artistSongLiNode.className = 'd-flex align-items-center pb-3'
         artistSongLiNode.classList.add('gap-10')
-        artistSongLiNode.innerHTML = `<img src="${track.md5_image}"/>
-                                         <p>${track.title}</p>
-                                         <div class=" d-flex justify-content-end gap-10">
-                                             <p class="time">${track.rank}</p>
-                                             <p class="listens">${Math.floor(track.duration / 60)+':'+track.duration%60}</p>
+        artistSongLiNode.innerHTML  = `   
+                                         <div class='d-flex align-items-center'>
+                                         <img id='track-image'style="width:40px; height: 40px"src="${track.album.cover_small}"/>
+                                         <p id='track-title'>${track.title}</p></div>
+                                         <div class="song-rank-and-duration d-flex justify-content-end flex-grow-1 gap-10">
+                                             <p class="time m-0">${track.rank}</p>
+                                             <p class="listens m-0">${Math.floor(track.duration / 60)+':'+track.duration%60}</p>
                                          </div>
                                          `
                                          ;
         songsOl.appendChild(artistSongLiNode)
     });
    
+   
     pageBgcontainer.style.backgroundImage = `url(${artist.picture_xl})`
+    let indexContainer = document.querySelectorAll('li div:nth-child(1)')
+    for (let index = 0; index < indexContainer.length; index++) {
+        
+        let indexP = document.createElement('p')
+        indexP.setAttribute('id','track-index')
+        indexP.innerText = `${index+1}`
+        indexContainer[index].appendChild(indexP)
+        
+        
+    }
     
 }
 
-const displayArtistPick = function (artist,tracklist) {
-     let artistPickImg = document.createElement('img')
-     artistPickImg.setAttribute('src',object.img)
-     artistPickDivNode.appendChild(artistPickImg)
-     let artistPickInfo = document.createElement('div')
-     artistPickInfo.className = 'd-flex flex-column'
-     artistPickInfo.innerHTML = `<p>${object.posted}</p>
-                                <h3>${object.TBD}</h3>
-                                <p>${object.TBD}<p>`
+const displayArtistPick = function (artist,album) {
+     let artistPickImg = document.getElementById('artist-pick-img')
+     let artistPickImgRoundedSmall = document.getElementById('artist-pick-img-rounded-small')
+     artistPickImgRoundedSmall.setAttribute('src',artist.picture_small)
+     artistPickImg.setAttribute('src',album.cover_small)
+     let artistPickName = document.getElementById('artist-posted-by')
+     artistPickName.innerText += " " + artist.name
+     let artistPickBestOf = document.getElementById('artist-pick-bestof')
+     artistPickBestOf.innerText = artist.name + " " + "Best Of"
+    
 }
 
 const getTracklist = async function (url) {
@@ -60,5 +75,10 @@ const getTracklist = async function (url) {
     const tracklist = await response.json()
     // const songs = artist.songs
    console.log(tracklist) 
+   console.log(tracklist.data[0].album)
    return tracklist.data
 }
+
+getArtist(url)
+
+
